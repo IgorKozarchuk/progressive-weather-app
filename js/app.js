a8d458a30727a37bb0c3f8f5ceb260a9dae2be4d
IgorKozarchuk <igor.a.kozarchuk@gmail.com>
2017-12-20 19:17:03 +0200

    upd

--- js/app.js
+++ js/app.js
@@ -371,7 +371,7 @@
 	// Service worker
 	if ("serviceWorker" in navigator) {
 		navigator.serviceWorker
-			.register("../service-worker.js")
+			.register("./service-worker.js")
 			.then(function() {
 				console.log("Service Worker Registered");
 			});
